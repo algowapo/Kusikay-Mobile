b16a82f2802eee2c5efb9d99ecb19686ebb69544
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-12-11 10:49:35 -0500

    Testing schedule_card.

--- lib/pages/teacher/schedule_teacher.dart
+++ lib/pages/teacher/schedule_teacher.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:kusikay_mobile/widgets/schedule_card.dart';
 
 class ScheduleTeacher extends StatelessWidget {
   const ScheduleTeacher({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Center(
         child: Column(
@@ -32,7 +34,15 @@ class ScheduleTeacher extends StatelessWidget {
                 'Schedule',
                 style: Theme.of(context).textTheme.overline,
               ),
-            )
+            ),
+            const ScheduleCard(
+                leader: true,
+                date: '3 Agosto',
+                time: '09:00-10:00',
+                title: 'Reunion Mensual',
+                icon: Icon(Icons.groups_outlined),
+                description:
+                    'Se coordinarán detalles sobre el mes. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.')
           ],
         ),
       ),
