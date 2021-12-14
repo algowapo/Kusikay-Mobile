52b818f759ec83a91ab029f290226ee72e09a068
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-12-14 17:42:26 -0500

    Self-contained backdrop and opacity.

--- lib/widgets/benefits_tab.dart
+++ lib/widgets/benefits_tab.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:kusikay_mobile/widgets/base_card.dart';
 import 'package:kusikay_mobile/widgets/vertical_separator.dart';
 
-//TODO: Refactor to self-contain the opacity and backdrop
-
 class BenefitsTab extends StatelessWidget {
-  BenefitsTab({Key? key}) : super(key: key);
+  final bool showingBenefits;
+  final VoidCallback onDismiss;
+  BenefitsTab(
+      {Key? key, required this.showingBenefits, required this.onDismiss})
+      : super(key: key);
+
+  //TODO: BenefitService
   final List<List<String>> benefitsList = [
     [
       'assets/bembos_logo.png',
@@ -20,45 +24,72 @@ class BenefitsTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: EdgeInsets.only(top: width * 0.06),
-      child: BaseCard(
-          child: ListView.separated(
-              shrinkWrap: true,
-              itemBuilder: (BuildContext context, int index) => Padding(
-                    padding: EdgeInsets.all(width * 0.02),
-                    child: Row(
-                      children: [
-                        Flexible(
-                          child: ClipRRect(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(width * 0.03)),
-                            child: Image.asset(
-                              benefitsList[index][0],
-                              height: width * 0.12,
-                            ),
-                          ),
-                          flex: 7,
-                        ),
-                        Flexible(
-                          child: Container(),
-                          flex: 2,
-                        ),
-                        Flexible(
-                          child: Text(
-                            benefitsList[index][1],
-                            style: const TextStyle(height: 1.7),
-                          ),
-                          flex: 12,
-                        )
-                      ],
-                    ),
+    return Stack(
+      alignment: AlignmentDirectional.topCenter,
+      children: [
+        GestureDetector(
+          onTap: onDismiss,
+          child: AnimatedOpacity(
+            opacity: showingBenefits ? 1 : 0,
+            duration: const Duration(milliseconds: 200),
+            child: Flex(
+              direction: Axis.vertical,
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.white.withOpacity(0.7),
                   ),
-              separatorBuilder: (BuildContext context, int index) => Padding(
-                    padding: EdgeInsets.symmetric(vertical: width * 0.06),
-                    child: const VerticalSeparator(),
-                  ),
-              itemCount: benefitsList.length)),
+                ),
+              ],
+            ),
+          ),
+        ),
+        AnimatedOpacity(
+          opacity: showingBenefits ? 1 : 0,
+          duration: const Duration(milliseconds: 200),
+          child: Padding(
+            padding: EdgeInsets.only(top: width * 0.06),
+            child: BaseCard(
+                child: ListView.separated(
+                    shrinkWrap: true,
+                    itemBuilder: (BuildContext context, int index) => Padding(
+                          padding: EdgeInsets.all(width * 0.02),
+                          child: Row(
+                            children: [
+                              Flexible(
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.all(
+                                      Radius.circular(width * 0.03)),
+                                  child: Image.asset(
+                                    benefitsList[index][0],
+                                    height: width * 0.12,
+                                  ),
+                                ),
+                                flex: 7,
+                              ),
+                              Flexible(
+                                child: Container(),
+                                flex: 2,
+                              ),
+                              Flexible(
+                                child: Text(
+                                  benefitsList[index][1],
+                                  style: const TextStyle(height: 1.7),
+                                ),
+                                flex: 12,
+                              )
+                            ],
+                          ),
+                        ),
+                    separatorBuilder: (BuildContext context, int index) =>
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: width * 0.06),
+                          child: const VerticalSeparator(),
+                        ),
+                    itemCount: benefitsList.length)),
+          ),
+        ),
+      ],
     );
   }
 }
