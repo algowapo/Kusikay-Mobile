523fdf86c5fc610c277fdbc577372fea3c0828a5
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2022-01-14 16:43:06 -0500

    Definida la estructura base para Leader

---
+++ lib/pages/leader/my_reports_leader.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyReportsLeader extends StatelessWidget {
+  const MyReportsLeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('hola mis reportes'),
+    );
+  }
+}
---
+++ lib/pages/leader/schedule_leader.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ScheduleLeader extends StatelessWidget {
+  const ScheduleLeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('hola mi horario'),
+    );
+  }
+}
