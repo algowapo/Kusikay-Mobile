50e38c606dbe4d43647525f275cb0cd45e4dc574
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2022-01-15 00:36:32 -0500

    Dialog de Attendance Creado

--- lib/pages/leader/schedule_leader.dart
+++ lib/pages/leader/schedule_leader.dart
@@ -1,12 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animated_dialog/flutter_animated_dialog.dart';
+import 'package:kusikay_mobile/widgets/attendance_dialog.dart';
+import 'package:kusikay_mobile/widgets/schedule_card.dart';
+import 'package:kusikay_mobile/widgets/vertical_separator.dart';
+import 'package:kusikay_mobile/widgets/week_selector.dart';
+import 'package:kusikay_mobile/widgets/year_month_viewer.dart';
 
 class ScheduleLeader extends StatelessWidget {
   const ScheduleLeader({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('hola mi horario'),
+    double width = MediaQuery.of(context).size.width;
+
+    EdgeInsets listPadding =
+    EdgeInsets.fromLTRB(width * 0.06, 0, width * 0.06, width * 0.03);
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const YearMonthViewer(
+              year: '2021',
+              month: 'Agosto',
+            ),
+            const VerticalSeparator(),
+            const WeekSelector(
+              dateStrings: ['2-8', '9-15', '16-22', '23-29', '30-5'],
+            ),
+            const VerticalSeparator(),
+            Expanded(
+              child: ListView(
+                physics: const BouncingScrollPhysics(),
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(width * 0.06),
+                    child: InkWell(
+                      child: const ScheduleCard(
+                          date: '3 Agosto',
+                          time: '09:00-10:00',
+                          title: 'Reunion Mensual',
+                          icon: Icon(Icons.groups_outlined),
+                          description:
+                          'Se coordinarán detalles sobre el mes. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.'),
+                      onTap: () => showAnimatedDialog(
+                          context: context,
+                        builder: (BuildContext context){
+                          return AttendanceDialog();
+                        },
+                        animationType: DialogTransitionType.slideFromBottom,
+                        curve: Curves.fastOutSlowIn,
+                        duration: Duration(milliseconds: 500),
+                        barrierDismissible: true,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: listPadding,
+                    child: const ScheduleCard(
+                        date: '4 Agosto',
+                        time: '12:00-01:00',
+                        title: 'Clase de Artes Libres',
+                        icon: Icon(Icons.school_outlined),
+                        description:
+                        'Se coordinarán detalles sobre el mes. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.'),
+                  ),
+                  Padding(
+                    padding: listPadding,
+                    child: const ScheduleCard(
+                        date: '4 Agosto',
+                        time: '12:00-01:00',
+                        title: 'Clase de Artes Libres',
+                        icon: Icon(Icons.school_outlined),
+                        description:
+                        'Se coordinarán detalles sobre el mes. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.'),
+                  ),
+                  Padding(
+                    padding: listPadding,
+                    child: const ScheduleCard(
+                        date: '4 Agosto',
+                        time: '12:00-01:00',
+                        title: 'Clase de Artes Libres',
+                        icon: Icon(Icons.school_outlined),
+                        description:
+                        'Se coordinarán detalles sobre el mes. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/attendance_dialog.dart
@@ -0,0 +1,221 @@
+import 'package:flutter/material.dart';
+import 'package:kusikay_mobile/models/session_report.dart';
+import 'package:kusikay_mobile/widgets/cross_button.dart';
+
+class AttendanceDialog extends StatefulWidget {
+  const AttendanceDialog({Key? key}) : super(key: key);
+
+  @override
+  _AttendanceDialogState createState() => _AttendanceDialogState();
+}
+
+class _AttendanceDialogState extends State<AttendanceDialog> {
+
+  bool fieldsEnabled = true;
+  var duration = TextEditingController();
+  var textForm1 = TextEditingController();
+  var textForm2 = TextEditingController();
+
+  final teachers = [
+    "Hector Suzuki",
+    "Gino Quispe",
+    "Juan Hernandez",
+    "Valeria Duarte"
+  ];
+
+  void toggleEditing() {
+    setState(() {
+      fieldsEnabled = !fieldsEnabled;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(15))),
+      insetPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 80),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AssistanceDialogHeader(),
+            Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 10, left: 10),
+                  child: ListView.builder(
+                      padding: EdgeInsets.zero,
+                      shrinkWrap: true,
+                    itemCount: teachers.length,
+                      itemBuilder: (context, index) {
+                        return TeacherTile(teacher: teachers[index],);
+                      }
+                  ),
+                )
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(width: 2.0, color: Colors.grey.shade400),
+                ),
+                color: Colors.white,
+              ),
+              height: 60,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  fieldsEnabled
+                      ? InkWell(
+                    onTap: () {
+                      toggleEditing();
+                      print("Se descarto");
+                    },
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.cancel_outlined,
+                          size: 20,
+                        ),
+                        SizedBox(
+                          width: 3,
+                        ),
+                        Text('Cancelar',
+                            style: Theme.of(context).textTheme.subtitle1),
+                      ],
+                    ),
+                  )
+                      : InkWell(
+                    onTap: () {
+                      if (fieldsEnabled) {
+                        toggleEditing();
+                        print("no se puede editar");
+                      } else {
+                        toggleEditing();
+                        print('se puede editar');
+                      }
+                    },
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.edit,
+                          size: 20,
+                        ),
+                        SizedBox(
+                          width: 3,
+                        ),
+                        Text('Editar',
+                            style: Theme.of(context).textTheme.subtitle1),
+                      ],
+                    ),
+                  ),
+                  Container(
+                      height: 50, child: VerticalDivider(color: Colors.black38)),
+                  fieldsEnabled
+                      ? InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                      print("Se actualizo");
+                    },
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.check_circle_outline,
+                          size: 20,
+                        ),
+                        SizedBox(
+                          width: 3,
+                        ),
+                        Text('Registrar',
+                            style: Theme.of(context).textTheme.subtitle1),
+                      ],
+                    ),
+                  )
+                      : InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.check_circle_outline,
+                          size: 20,
+                        ),
+                        SizedBox(
+                          width: 3,
+                        ),
+                        Text('Salir',
+                            style: Theme.of(context).textTheme.subtitle1),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+      ),
+    );
+  }
+}
+
+
+class AssistanceDialogHeader extends StatelessWidget {
+  const AssistanceDialogHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Asistencia",
+              style: Theme.of(context).textTheme.headline1),
+
+          SizedBox(
+            height: 7,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("09:00-10:00",
+                  style: Theme.of(context).textTheme.headline2),
+              Text("3/8", style: Theme.of(context).textTheme.headline2)
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TeacherTile extends StatelessWidget {
+  final String teacher;
+  const TeacherTile({Key? key, required this.teacher}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    return Container(
+      width: width,
+      height: 60,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(teacher, style: Theme.of(context).textTheme.headline2),
+          Row(
+            children: [
+              Icon(Icons.lightbulb_outline_rounded, size: 30,),
+              SizedBox(width: 10,),
+              Icon(Icons.thumb_up_alt_outlined, size: 30),
+              SizedBox(width: 10,),
+              CrossButton(tapped: (){
+                print('hola');
+              })
+
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
