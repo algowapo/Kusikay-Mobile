b8531246189ae8f27c282c1d99521b826ed0efd0
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2022-02-17 19:34:34 -0500

    Trayendo todos los teachers by leaderId al revisar informes

--- lib/pages/leader/review_report_leader.dart
+++ lib/pages/leader/review_report_leader.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:kusikay_mobile/models/teacher.dart';
+import 'package:kusikay_mobile/services/teacher_service.dart';
 import 'package:kusikay_mobile/widgets/vertical_separator.dart';
 
-class ReviewReports extends StatelessWidget {
+class ReviewReports extends StatefulWidget {
   const ReviewReports({Key? key}) : super(key: key);
+
+  @override
+  State<ReviewReports> createState() => _ReviewReportsState();
+}
+
+class _ReviewReportsState extends State<ReviewReports> {
+  TeacherService teacherService = TeacherService();
+
+  List<Teacher> teachers = [];
+
+  void getData() async {
+    teachers = await teacherService.getTeachersByLeaderId(1);
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,17 +39,20 @@ class ReviewReports extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Danza Folclorica', style: Theme.of(context).textTheme.headline1),
+            //TODO: TRAER NOMBRE DEL CURSO AL LOGIN
+            Text('Danza Folclorica',
+                style: Theme.of(context).textTheme.headline1),
             Expanded(
-              child: ListView(
+              child: ListView.builder(
+                itemCount: teachers.length,
                 physics: const BouncingScrollPhysics(),
-                children: [
-                  RevisarInformeTile(alert: alert),
-                  const VerticalSeparator(),
-                  const RevisarInformeTile(alert: false),
-                  const VerticalSeparator(),
-                  RevisarInformeTile(alert: alert),
-                ],
+                itemBuilder: (context, index) {
+                  return RevisarInformeTile(
+                    alert: false,
+                    name: teachers[index].name!,
+                    phone: teachers[index].phone!,
+                  );
+                },
               ),
             )
           ],
@@ -38,21 +63,25 @@ class ReviewReports extends StatelessWidget {
 }
 
 class RevisarInformeTile extends StatelessWidget {
-  const RevisarInformeTile({
-    Key? key,
-    required this.alert,
-  }) : super(key: key);
+  const RevisarInformeTile(
+      {Key? key, required this.alert, required this.name, required this.phone})
+      : super(key: key);
 
   final bool alert;
+  final String name;
+  final String phone;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(2),
       child: ListTile(
-        title: const Text("Hector Suzuki"),
-        subtitle: const Text("984234567"),
-        trailing: alert == true ? const FaIcon(FontAwesomeIcons.bell, color: Colors.black87, size: 25) : null,
+        title: Text(name),
+        subtitle: Text(phone),
+        trailing: alert == true
+            ? const FaIcon(FontAwesomeIcons.bell,
+                color: Colors.black87, size: 25)
+            : null,
         onTap: () {
           //dirigir a vista de Calixtro
         },
